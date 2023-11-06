71f0f4bf01bc44ba8246c4a95d3871f406318130
KERRCAM <kerrcameron@hotmail.com>
2023-11-06 15:34:58 +0000

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/2dAssetPrototypingProgram.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "2dAssetPrototypingProgram"
+  "2dAssetPrototypingProgram.pdb"
+  "CMakeFiles/2dAssetPrototypingProgram.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2dAssetPrototypingProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
